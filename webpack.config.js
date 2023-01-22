e2bef96ada9f9c32b82b10adebe26da9ac936079
Vitalii Koreiev <vitaliikoreiev@gmail.com>
2023-01-22 11:04:11 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dest'),
+        filename: '[name].js'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dest')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true
+    },
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+            }
+        ]
+    }
+}
